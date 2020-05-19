3bca663d593e4801d4bd7e6f0bfeb3843d16d2e4
manali dey <manali.dey@pwc.com>
2020-05-19 16:50:25 +0530

    Final Commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(__dirname + '/dist/test'));
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/test/index.html'));
+});
+app.listen(process.env.PORT || 8000);
+console.log('Console listening');
